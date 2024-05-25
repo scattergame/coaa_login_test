b0baba0008c86b9a05de3e28b1128d5094a91f36
scattergame <90742519+scattergame@users.noreply.github.com>
2024-05-25 09:09:17 -0400

    first commit add login function

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,16 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 import "./globals.css";
+import { ThemeSwitcher } from "@/components/ThemeSwitcher";
+import { Providers } from "@/app/providers";
+import Appbar from "@/components/Appbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +25,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+        <html lang="en">
+        <body className={inter.className}>
+          <Providers>
+            <Appbar />
+              <div>
+              {children}
+              </div>
+            </Providers>
+          </body>
+        </html>
+    </ClerkProvider>
   );
 }
