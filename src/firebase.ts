5e1248501890b2b971f56a61f037853f522236ce
scattergame <90742519+scattergame@users.noreply.github.com>
2024-06-02 20:35:47 -0400

    fix env name

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,8 +5,8 @@ import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_APIKEY,
-  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
   databaseURL: process.env.FIREBASE_DATABASEURL,
   projectId: process.env.FIREBASE_PROJECTID,
   storageBucket: process.env.FIREBASE_STORAGEBUCKET,
@@ -14,6 +14,16 @@ const firebaseConfig = {
   appId: process.env.FIREBASE_APPID,
   measurementId: process.env.FIREBASE_MEASUREMENTID
 };
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA-EmgAVBbeAOgoZ2KDxSit8umvthsTIKs",
+//   authDomain: "nextjs-firebase-f66b6.firebaseapp.com",
+//   databaseURL: "https://nextjs-firebase-f66b6-default-rtdb.firebaseio.com",
+//   projectId: "nextjs-firebase-f66b6",
+//   storageBucket: "nextjs-firebase-f66b6.appspot.com",
+//   messagingSenderId: "319776246570",
+//   appId: "1:319776246570:web:613f1b1899748ecfcdac53",
+//   measurementId: "G-YLCMK4PNLP"
+// };
 
 // Connect to your Firebase app
 const app = initializeApp(firebaseConfig);
